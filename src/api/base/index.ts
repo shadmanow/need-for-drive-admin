be58c2810421c90268e2d8ad720839a3af877ae8
societybolt <shadmanow20@yandex.ru>
2021-10-04 16:19:59 +0400

    f-09 добавил обновление токена

--- src/api/base/index.ts
+++ src/api/base/index.ts
@@ -3,6 +3,7 @@ import cookies from 'react-cookies';
 
 import { store } from '@store/store';
 import { setError } from '@store/error/thunks';
+import { refreshToken } from '@api/auth';
 
 const baseApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
@@ -30,9 +31,25 @@ baseApi.interceptors.request.use(
 baseApi.interceptors.response.use(
   (response) => response,
   async (error) => {
+    const request = error.config;
+
     if (error.response?.status) {
       setError(error.response.status)(store.dispatch);
+
+      if (error.response.status === 401) {
+        const user = cookies.load('user');
+        if (user?.refreshToken) {
+          const auth = await refreshToken(user.refreshToken);
+          cookies.save('user', { ...auth }, { path: '/' });
+          request.headers = {
+            ...request.headers,
+            Authrorization: `Bearer ${auth.accessToken}`
+          };
+          return baseApi(request);
+        }
+      }
     }
+
     return Promise.reject(error);
   }
 );
